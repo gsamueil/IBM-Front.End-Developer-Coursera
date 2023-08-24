50b996d00c1f62a37f5db233cb4975cd39321d76
George Samuel Mahrous <31563545+gsamueil@users.noreply.github.com>
2023-08-25 01:09:44 +0300

    Create simple_interest.sh

---
+++ IBM-Git-and-Github-project/simple_interest.sh
@@ -0,0 +1,42 @@
+#Simple Interest Calculator
+This is a simple Python script that calculates the simple interest for a given principal amount, interest rate, and time period.
+
+How to Use
+
+Sure! Below is a simple "README.md" file for a GitHub project that calculates and displays the simple interest for a given principal amount, interest rate, and time period.
+
+Simple Interest Calculator
+This is a simple Python script that calculates the simple interest for a given principal amount, interest rate, and time period.
+
+How to Use
+Clone this repository to your local machine:
+
+bash
+Copy code
+git clone https://github.com/your_username/simple-interest-calculator.git
+Navigate to the project directory:
+
+bash
+Copy code
+cd simple-interest-calculator
+Run the Python script:
+
+bash
+Copy code
+python simple_interest.py
+Enter the principal amount, interest rate, and time period when prompted.
+
+The script will calculate and display the simple interest.
+
+Example
+Let's say you want to calculate the simple interest for a principal amount of $1000 with an interest rate of 5% for 3 years. After running the script and providing the required inputs, you will get the following output:
+
+mathematica
+Copy code
+Enter the principal amount: 1000
+Enter the interest rate (in percentage): 5
+Enter the time period (in years): 3
+
+Simple Interest: $50.00
+
+# Path: simple_interest.sh
